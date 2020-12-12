9610f95be0bb92740675c4c6eab87dbc1c705e29
yeongmi yoon <35258834+maie421@users.noreply.github.com>
2020-12-12 23:41:19 +0900

    Update talk_amk.py

--- talk_amk.py
+++ talk_amk.py
@@ -6,13 +6,13 @@ def main():
     while True:
         if voice.detect_wake_up_word():
             input_text = voice.get_text_from_voice()
-            print(input_text)
-            if input_text.find("날씨") != -1:
+            print(input_text) #voice 출력 문자열
+            if input_text.find("날씨") != -1: #날씨 문자열이 있을겨우
                     weather.main()
-            elif input_text.find("냉장고") != -1:
+            elif input_text.find("냉장고") != -1:#냉장고 문자열이 있을겨우
                    food_recognize.main()
             else:
-                result_answer = voice.query_by_text(input_text)
-                voice.speech(result_answer)
+                result_answer = voice.query_by_text(input_text) 
+                voice.speech(result_answer) #다시 말씀해 주세요
 if __name__ == "__main__":
     main()
