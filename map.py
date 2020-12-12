dd08ca2984c47c4befb4e2535f8af6657bc64781
yeongmi yoon <35258834+maie421@users.noreply.github.com>
2020-12-12 23:49:07 +0900

    Update map.py

--- map.py
+++ map.py
@@ -2,12 +2,12 @@ import requests
 
 def get_coord_by_address(address):
     api_url = 'https://nominatim.openstreetmap.org/search?q=%s&format=json&countrycodes=kr'
-    response = requests.get(api_url % address)
-    if response.status_code == 200:
+    response = requests.get(api_url % address) #get 방식으로 데이터 가져옴
+    if response.status_code == 200: #성공 할경우
         json = response.json()
-        if len(json) > 0:
-            first = json[0]
-            return (first['lat'], first['lon'])
+        if len(json) > 0: #json 길이가 0 이상
+            first = json[0] #데이터 가져옴
+            return (first['lat'], first['lon']) #lat 와 lon 데이터 가져옴
         else:
             return None
     else:
